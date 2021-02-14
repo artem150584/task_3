d0f24d893653cb68a94212de64bebb8aac2b5aa1
artem <artem150584@mail.ru>
2021-02-14 21:53:01 +0700

    author from DB

--- src/main/java/com/example/MyBookShopApp/controllers/AuthorController.java
+++ src/main/java/com/example/MyBookShopApp/controllers/AuthorController.java
@@ -1,10 +1,15 @@
 package com.example.MyBookShopApp.controllers;
 
+import com.example.MyBookShopApp.data.Author;
 import com.example.MyBookShopApp.data.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class AuthorController {
@@ -16,14 +21,13 @@ public class AuthorController {
         this.authorService = authorService;
     }
 
-//    @GetMapping("/index.html")
-//    public String mainPage() {
-//        return "redirect:/bookshop/main";
-//    }
+    @ModelAttribute("authorMap")
+    public Map<String, List<Author>> authorsMap() {
+        return authorService.getAuthorsMap();
+    }
 
     @GetMapping("/authors")
-    public String authorsPage(Model model) {
-//        model.addAttribute("authorData", authorService.getAuthorData());
+    public String authorsPage() {
         return "authors/index";
     }
 }
--- src/main/java/com/example/MyBookShopApp/data/AuthorService.java
+++ src/main/java/com/example/MyBookShopApp/data/AuthorService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class AuthorService {
@@ -18,15 +20,19 @@ public class AuthorService {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public List<Author> getAuthorData() {
-        List<Author> authors = jdbcTemplate.query("SELECT * FROM authors ORDER BY name", (ResultSet rs, int rowNum) -> {
+    public Map<String, List<Author>> getAuthorsMap() {
+
+        List<Author> authors = jdbcTemplate.query("SELECT * FROM authors", (ResultSet rs, int rowNum) -> {
             Author author = new Author();
-            author.setId(rs.getInt("author_id"));
-            author.setName(rs.getString("name"));
+            author.setId(rs.getInt("id"));
+            author.setFirstName(rs.getString("first_name"));
+            author.setLastName(rs.getString("last_name"));
 
             return author;
         });
 
-        return new ArrayList<>(authors);
+        return authors.stream().collect(Collectors.groupingBy((Author a) -> {
+            return a.getLastName().substring(0, 1);
+        }));
     }
 }
