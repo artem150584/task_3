fe87ccf036cf5eccf8fff56bc313d8800a3527ac
artem <artem150584@mail.ru>
2021-02-16 23:54:39 +0700

    task 1 except Search

--- src/main/java/com/example/MyBookShopApp/controllers/FaqController.java
+++ src/main/java/com/example/MyBookShopApp/controllers/FaqController.java
@@ -1,16 +1,20 @@
 package com.example.MyBookShopApp.controllers;
 
+import com.example.MyBookShopApp.data.Author;
 import com.example.MyBookShopApp.data.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
-public class GenresPagesController {
+public class FaqController {
 
-//    @GetMapping("/genres/index.html")
-//    public String genresPage() {
-//        return "genres/index";
-//    }
+    @GetMapping("/faq")
+    public String faqPage() {
+        return "faq";
+    }
 }
