d113531176c579b1dbcf674ba689db9caa06dccd
artem <artem150584@mail.ru>
2021-02-23 14:26:25 +0700

    switch DB to postgre

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,17 +1,19 @@
 DROP TABLE IF EXISTS books;
 DROP TABLE IF EXISTS authors;
 
-CREATE TABLE  books(
-   id INT AUTO_INCREMENT PRIMARY KEY,
-   author VARCHAR(250) NOT NULL,
-   title VARCHAR(250) NOT NULL,
-   priceOld  VARCHAR(250) DEFAULT NULL,
-   price VARCHAR(250) DEFAULT NULL
+CREATE TABLE books
+(
+    id       BIGSERIAL PRIMARY KEY,
+    author   VARCHAR(250) NOT NULL,
+    title    VARCHAR(250) NOT NULL,
+    priceOld VARCHAR(250) DEFAULT NULL,
+    price    VARCHAR(250) DEFAULT NULL
 );
 
-create table authors (
-     id INT,
-     first_name VARCHAR(50),
-     last_name VARCHAR(50)
+create table authors
+(
+    id         BIGSERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name  VARCHAR(50)
 );
 
